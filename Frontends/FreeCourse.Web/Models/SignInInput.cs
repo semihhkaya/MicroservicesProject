572f92d27a4cfc7873c07ec6bedbe21433d9f7e2
DELL <DELL@SEMIH>
2023-05-26 23:37:14 +0300

    MVC Client added.

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput //kullanıcdan data alcaksak input kullanıcya data göstereceksek viev model türünde class isimlendirmesi yapalaım
+    {
+        [Display(Name = "Email adresiniz")] 
+        public string Email { get; set; }
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signInInput); //IdentityModel kütüphaneisnden yararlanıcaz
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken(); //kullanıcı logout olduğunda onun için tanımlı token kaldırılacak.
+    }
+}
